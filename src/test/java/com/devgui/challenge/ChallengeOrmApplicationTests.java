0fde6b42866ca474992e41cf0e232eec627ecb4f
GuilhermeBarroso-sys <guibarrosodeoliveira5@gmail.com>
2024-08-15 22:32:43 -0300

    Finishing challenge

---
+++ src/test/java/com/devgui/challenge/ChallengeOrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devgui.challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeOrmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
